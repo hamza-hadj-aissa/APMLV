1b899c7bc5c178dcb0d3f538c9c59caea2547c09
Hamza HADJ AISSA <hadjaissahamza@gmail.com>
2023-12-01 23:55:44 +0100

    feat(LSTM): loaded model checkpoint and tested on a new dataset

--- prediction/test_model.py
+++ prediction/test_model.py
@@ -1,4 +1,4 @@
-from lstm import data_preprocessing, TimeSeriesDataset, scaler, LOOKBACK
+from lstm import data_preprocessing, TimeSeriesDataset, scaler, LOOKBACK, LSTM
 import pandas as pd
 from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
@@ -12,13 +12,20 @@ import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
-model = torch.load(
-    "/home/hamza/Desktop/studio/python/lvm_balancer/data_generation/models/model_4fa5dc13-ab08-44d4-a44f-5876e96ab242.pth")
+learning_rate = 0.01
+model = LSTM(1, 4, 1)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
+
+loaded_state = torch.load(
+    "/home/hamza/Desktop/studio/python/lvm_balancer/prediction/models/lstm_checkpoint_4fa5dc13-ab08-44d4-a44f-5876e96ab242.pt")
+model.load_state_dict(loaded_state["model_state"])
+optimizer.load_state_dict(loaded_state["optimizer"])
+model.eval()
 df = pd.read_csv(
-    "/home/hamza/Desktop/studio/python/lvm_balancer/data_generation/dataset/test_logical_volume.csv")
+    "/home/hamza/Desktop/studio/python/lvm_balancer/prediction/dataset/test_logical_volume.csv")
 train_data, test_data = data_preprocessing(
-    df,
+    df.iloc[180:360],
     features=["used_space", "uuid"]
 )
 batch_size = 16
@@ -34,3 +41,13 @@ dummies = np.zeros((test_dataset.X.shape[0], LOOKBACK+1))
 dummies[:, 0] = test_predictions
 dummies = scaler.inverse_transform(dummies)
 test_predictions = dc(dummies[:, 0])
+
+
+dummies = np.zeros((test_dataset.X.shape[0], LOOKBACK+1))
+dummies[:, 0] = test_dataset.y.flatten()
+dummies = scaler.inverse_transform(dummies)
+actual = dc(dummies[:, 0])
+plt.plot(test_predictions, label="Predictions")
+plt.plot(actual, label="Actual")
+plt.legend()
+plt.show()
