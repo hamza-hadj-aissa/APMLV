2226848aa647bc9ecd7585f742dd4e90a1345cef
Hamza HADJ AISSA <hadjaissahamza@gmail.com>
2023-12-08 22:14:33 +0100

    fix(DB): Database drop script

--- database/drop.py
+++ database/drop.py
@@ -1,8 +1,10 @@
 
 
 from database.connect import connect_to_database, create_tables, drop_tables
-
-
-engine = connect_to_database()
-drop_tables(engine)
-create_tables(engine)
+from logs.Logger import Logger
+from root_directory import root_directory
+log_file_path = f"{root_directory}/logs/lvm_balancer.log"
+db_logger = Logger("Postgres", path=log_file_path)
+engine = connect_to_database(logger=db_logger)
+drop_tables(engine, db_logger)
+create_tables(engine, db_logger)
