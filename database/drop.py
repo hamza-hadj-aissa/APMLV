8de94662a6f2270cdfe8a5d583b9fe46316644f4
Hamza HADJ AISSA <hadjaissahamza@gmail.com>
2023-12-30 13:52:18 +0100

    Refactor database drop script and logger initialization

--- database/drop.py
+++ database/drop.py
@@ -2,9 +2,8 @@
 
 from database.connect import connect_to_database, create_tables, drop_tables
 from logs.Logger import Logger
-from root_directory import root_directory
-log_file_path = f"{root_directory}/logs/lvm_balancer.log"
-db_logger = Logger("Postgres", path=log_file_path)
+
+db_logger = Logger("Postgres")
 engine = connect_to_database(logger=db_logger)
 drop_tables(engine, db_logger)
 create_tables(engine, db_logger)
